2779408c8738a7c787c6e8e055dfd7946dcf39cc
Adrian Średniawski <sredni98123@gmail.com>
2023-08-31 22:02:44 +0200

    Add working links

--- js/script.js
+++ js/script.js
@@ -37,10 +37,60 @@ function titleClickHandler(event){
     
     targetArticle.classList.add('active');
 
+  
+
   }
   
+
+
+//GENEROWANIE LISTY TYTUŁÓW
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+const titleList = document.querySelector(optTitleListSelector);
+titleList.innerHTML = '';
+
+const articles = document.querySelectorAll(optArticleSelector);
+console.log(articles);
+
+
+let html = '';
+  
+  for(let article of articles){
+
+    /*[DONE] get the article id */
+
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+     /* insert link into titleList */
+     html = html + linkHTML;
+
+     console.log(html);
+  }
+    
+  titleList.innerHTML = html;
+
   const links = document.querySelectorAll('.titles a');
+  console.log('asd'+links);
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
+
+generateTitleLinks();
